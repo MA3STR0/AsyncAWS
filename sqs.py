712c611441cfa66c007d509c0d85f525af69d74e
Anton <git@maestr0.com>
2014-03-13 12:15:09 +0100

    Add listen-queue method for SQS

--- sqs.py
+++ sqs.py
@@ -61,4 +61,21 @@ class SQS(object):
         self.__secret_key = secret_key
         self._http = AsyncHTTPClient()
 
+    def listen_queue(self, queue_uri):
+        params = {
+            "Action": "ReceiveMessage",
+            "WaitTimeSeconds": 15,
+            "MaxNumberOfMessages": 1,
+            "VisibilityTimeout": 300,
+            "AttributeName": "All",
+            "Version": "2012-11-05",
+            "SignatureMethod": "HmacSHA256",
+            "SignatureVersion": 4,
+        }
+        full_url = url_concat(queue_uri, params)
+        request = SQSRequest(full_url,
+                             access_key=self.__access_key,
+                             secret_key=self.__secret_key)
+        return self._http.fetch(request)
+
 
