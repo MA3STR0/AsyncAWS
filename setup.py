7328f2cb3e15514a1b65ba481e39c39d77660be8
Anton <git@maestr0.com>
2014-06-23 16:01:45 +0200

    Add package setup files

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(
+    name = 'asyncaws',
+    packages = ['asyncaws'],
+    version = '0.1.0',
+    description = 'Asynchronous AWS library in Python',
+    author = 'Anton Caceres',
+    author_email = 'm@e5t.ro',
+    url = 'http://caceres.me/asyncaws',
+    download_url = 'https://github.com/MA3STR0/asyncaws/tarball/v0.1',
+    keywords = ['aws', 'sqs', 'sns', 'tornado', 'boto'],
+    classifiers = [],
+    install_requires=[
+        "tornado",
+        "lxml"
+    ],
+)
